32cbecbff9ecba011db96ca7f9e4753db2404712
affan14 <affan.ahmad@yahoo.de>
2024-10-07 21:51:58 +0200

    Created python files main.py and game_utils.py

---
+++ game_utils.py
@@ -0,0 +1,16 @@
+
+
+
+def solve_riddle(riddle):  # type: ignore # Anzeige eines Rätsels und Eingabeaufforderung um es zu lößen.
+
+
+
+
+def end_game(): # type: ignore # Auswahl bzw. Eingabeaufforderung zum Beenden des Spiels. \n
+    print("Would you like to quit the game? n\
+          Please enter Y or N")
+    if c
+
+
+
+
---
+++ main.py
@@ -0,0 +1,25 @@
+# Adventure Game: The Lost Treasure Hunt
+from game_utils import solve_riddle, end_game   # Funktionen aus Datei game_utils importiert.
+
+
+def greeting():   # type: ignore 
+            # Definiere Funktion greeting (Begrüßung des Spielers und Anleitung)
+
+    print("Welcome to the 'Lost Treasure Hunt!' n\
+           Navigate through the rooms and solve the riddles n\
+           to find the Lost treasure")
+# Allgemeine Begrüßung und Erläuterung des Spiels.
+
+
+def enter_room(room): # type: ignore # Funktion mit der, der Spieler einen Raum betritt und weiter läüft.
+    print("You have entered the Main Entrance Hall. n\
+           In front of you is the majestic stairway to the rooms upstairs")
+
+
+
+
+
+
+
+
+
